1e26eac1d589785437030a97ae5ae62a0548f599
Suyash Mishra <smishra27@myseneca.ca>
2025-02-12 01:38:29 -0500

    Added lab3b

---
+++ lab3b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+'''The purpose of this script is to create a Python function that can return the free disk space on a Linux system's root directory'''
+
+# Author ID: [seneca_id]
+
+def sum_numbers(number1, number2):
+
+    return int(number1) + int(number2)
+
+def subtract_numbers(number1, number2):
+
+    return int(number1) - int(number2)
+
+def multiply_numbers(number1, number2):
+
+    return int(number1) * int(number2)
+
+if __name__ == '__main__':
+
+    print(sum_numbers(10, 5))       # Expected output: 15
+
+    print(subtract_numbers(10, 5))  # Expected output: 5
+
+    print(multiply_numbers(10, 5))  # Expected output: 50
+
+
